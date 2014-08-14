1d7d72a1a362dba53d0225f6e0a68f6e95cf204e
Sonny T <mail@sonnyt.com>
2014-08-14 16:12:31 -0400

    Added login

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -6,6 +6,19 @@
     *
     * Main module that loads everything
     */
-    angular.module('App', [ 'ngCookies', 'ngResource', 'ui.router', 'ui.bootstrap', 'User' ]);
+    angular.module('App', [ 'ngCookies', 'ngResource', 'ui.router', 'ui.bootstrap', 'User' ])
 
+    .run([ '$rootScope', '$location', function( $rootScope, $location ) {
+        /**
+         * Change <body> class
+         * it sets state name and action
+         */
+        $rootScope.$on( '$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            if ( toState.name ) {
+                var pieces = toState.name.split( '.' );
+
+                $rootScope.pageClass = pieces.join(' ');
+            }
+        });
+    } ]);
 })( angular );
