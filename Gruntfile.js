4ecf4c817537490feee60d68a8732de5c0ade847
Sonny T. <mail@sonnyt.com>
2014-12-01 11:49:44 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,57 +45,22 @@ module.exports = function(grunt) {
         copy: {
             bower: {
                 files: [
-                    // jQuery
-                    {
-                        src: 'app/bower_components/jquery/jquery.js',
-                        dest: 'app/js/library/jquery.js'
-                    },
-                    // Angular
-                    {
-                        src: 'app/bower_components/angular/angular.js',
-                        dest: 'app/js/library/angular.js'
-                    },
-                    // Angular Bootstrap UI
-                    {
-                        src: 'app/bower_components/angular-bootstrap/ui-bootstrap.js',
-                        dest: 'app/js/library/ui-bootstrap.js'
-                    },
-                    // Angular Bootstrap UI Template
-                    {
-                        src: 'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-                        dest: 'app/js/library/ui-bootstrap-tpls.js'
-                    },
-                    // Angular Cookies
-                    {
-                        src: 'app/bower_components/angular-cookies/angular-cookies.js',
-                        dest: 'app/js/library/angular-cookies.js'
-                    },
-                    // Angular Resource
-                    {
-                        src: 'app/bower_components/angular-resource/angular-resource.js',
-                        dest: 'app/js/library/angular-resource.js'
-                    },
-                    // Angular UI Router
-                    {
-                        src: 'app/bower_components/angular-ui-router/release/angular-ui-router.js',
-                        dest: 'app/js/library/angular-ui-router.js'
-                    },
                     // Bootstrap Fonts
                     {
                         expand: true,
                         cwd: 'app/bower_components/bootstrap-sass/fonts',
                         src: ['**/*'],
-                        dest: 'public/app/fonts',
+                        dest: 'public/app/fonts/bootstrap',
                         filter: 'isFile'
                     },
                     // FontAwesome Fonts
                     {
                         expand: true,
-                        cwd: 'frontend/bower_components/fontawesome/fonts',
+                        cwd: 'app/bower_components/fontawesome/fonts',
                         src: ['**/*'],
-                        dest: 'public/cdn-static/dist/app/fonts/fontawesome',
+                        dest: 'public/app/fonts/fontawesome',
                         filter: 'isFile'
-                    },
+                    }
                ]
             },
 
@@ -113,35 +78,28 @@ module.exports = function(grunt) {
             }
         },
 
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: [
-                    // library
-                    'app/js/library/jquery.js',
-                    'app/js/library/angular.js',
-                    'app/js/library/ui-bootstrap.js',
-                    'app/js/library/*.js',
-
-                    // app
-                    'app/js/src/app.js',
-                    'app/js/src/**/*.module.js',
-                    'app/js/src/**/*.js'
-                ],
-                dest: 'public/app/javascript/app.min.js'
-            }
-        },
-
         uglify: {
             angular: {
                 options: {
                     banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-                    mangle: false
+                    sourceMap: true
                 },
                 files: {
-                    'public/app/javascript/app.min.js': ['public/app/javascript/app.min.js']
+                    'public/app/javascript/app.min.js': [
+                        // libraries
+                        'app/bower_components/jquery/jquery.js',
+                        'app/bower_components/angular/angular.js',
+                        'app/bower_components/angular-bootstrap/ui-bootstrap.js',
+                        'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+                        'app/bower_components/angular-cookies/angular-cookies.js',
+                        'app/bower_components/angular-resource/angular-resource.js',
+                        'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+
+                        // app
+                        'app/js/src/app.js',
+                        'app/js/src/**/*.module.js',
+                        'app/js/src/**/*.js'
+                    ]
                 }
             }
         },
@@ -182,8 +140,8 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('serve', function (target) {
-        grunt.task.run(['jshint', 'jscs', 'clean-public', 'copy', 'concat', 'uglify', 'compass', 'express:dev', 'watch']);
+        grunt.task.run(['jshint', 'jscs', 'clean-public', 'copy', 'uglify', 'compass', 'express:dev', 'watch']);
     });
 
-    grunt.registerTask('default', ['jshint', 'jscs', 'clean-public', 'copy', 'concat', 'uglify', 'compass']);
+    grunt.registerTask('default', ['jshint', 'jscs', 'clean-public', 'copy', 'uglify', 'compass']);
 };
